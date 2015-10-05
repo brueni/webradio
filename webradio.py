5e1df00b8c6d63bc10c4521e121c35d54887b7cd
brueni <info@michael-bruenisholz.ch>
2015-10-05 22:29:45 +0200

    Added support for status LED

--- webradio.py
+++ webradio.py
@@ -6,18 +6,33 @@ from time import sleep
 import RPi.GPIO as GPIO
  
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
-GPIO.setup(25, GPIO.IN)
- 
+GPIO.setup(23, GPIO.IN) #Button Next
+GPIO.setup(24, GPIO.IN) #Button Play
+GPIO.setup(25, GPIO.IN) #Button Stop
+GPIO.setup(18, GPIO.OUT) #LED Red
+GPIO.setup(22, GPIO.OUT) #LED Green
+
+GPIO.output(18,False) #Set both LED to off
+GPIO.output(22,False) #in case of aborted script
+
+GPIO.output(18,True) #Set to Red
+
 while True:
     if (GPIO.input(24) == False):
         os.system('./play.sh &')
+	GPIO.output(18,False) #Set to Green
+	GPIO.output(22,True)	
 
     if (GPIO.input(25) == False):
         os.system('./stop.sh &')
- 
+	GPIO.output(18,True) #Set to Red
+	GPIO.output(22,False) 
+
     if (GPIO.input(23)== False):
+	GPIO.output(18,True) #Set to Orange
+	GPIO.output(22,True)
         os.system('./next.sh &')
+	sleep(1.5)		#Wait...
+	GPIO.output(18,False)	#And green Again
  
     sleep(0.5);
